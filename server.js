1fbc56061e94824cd91fff65c5f140efd9dd00bb
soumen baidya <soumenbaidyaheart@gmail.com>
2018-02-20 20:47:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two/article-four.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'ui/article-two/article-four.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
