955bf97f354c86a200a95350bd19d6046e140357
soumen baidya <soumenbaidyaheart@gmail.com>
2018-02-26 22:10:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,27 @@ var img = document.getElementById('madi');
 img.onclick = function () {
   img.style.marginLeft = '100px'; 
 };
+
+
+// counter  code
+
+
+var button = document.getElementById(counter);
+var counter = 0;
+
+button.onclick = function() {
+    //  Make a  request to the counter end point
+    
+    // Capture the response and store it in a variable
+    
+    // render the variable in the correct span
+    
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+    
+    
+    
+};
+
