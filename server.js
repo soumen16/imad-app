566bdacd1f71a3bd620eb0101b03cabfa5d613f3
soumen baidya <soumenbaidyaheart@gmail.com>
2018-02-28 06:00:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,14 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
+
 app.get('/:articleName', function (req, res) {
     // articleNmae == article-one
     // articles[articleName] == {} content object for article one
@@ -106,11 +114,7 @@ app.get('/article-three', function (req, res) {
 
 --------------------
 */ 
-var counter = 0;
-app.get('/counter', function (req, res) {
-    counter =  counter + 1;
-    res.send(counter.toString());
-});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
