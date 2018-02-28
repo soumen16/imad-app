c565e583636f2cb1cd5d3e88e21724c564f674cc
soumen baidya <soumenbaidyaheart@gmail.com>
2018-02-28 06:11:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,9 +32,5 @@ button.onclick = function() {
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
-    
-    
-    
-    
-};
+    };
 
