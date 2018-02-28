51d855e7d17247fdc82215f3c8367683f1e5853b
soumen baidya <soumenbaidyaheart@gmail.com>
2018-02-28 06:12:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,18 @@
-console.log('Loaded!');
+//console.log('Loaded!');
 
 
 //change the text of the main-text div 
 
-var element = document.getElementById('main-text');
+//var element = document.getElementById('main-text');
 
-element.innerHTML = 'Hello This Is New Html Text';
+//element.innerHTML = 'Hello This Is New Html Text';
 
 //moving the image
 
-var img = document.getElementById('madi');
-img.onclick = function () {
-  img.style.marginLeft = '100px'; 
-};
+//var img = document.getElementById('madi');
+//img.onclick = function () {
+//  img.style.marginLeft = '100px'; 
+//};
 
 
 // counter  code
