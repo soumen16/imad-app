a08d018600d6cbc329779861b86f4d3f6f2282f3
soumen baidya <soumenbaidyaheart@gmail.com>
2018-02-28 07:11:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,4 +50,51 @@ button.onclick = function() {
     request.send(null);
     
     };
+    
+    
+    // submit name
+    
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    // make a request to the server and the name
+    
+    //capture a list of names and render it as a list
+    var names =['name1','name2','name3'];
+    var list ='';
+    for (i=0; i<names.length; i++) {
+        list += '<li>' + names[i] + '<li>';
+        
+    }
+    
+    var ul = document.getElementById('namelist');
+    ui.innerHTML = list;
+};
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
