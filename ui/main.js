a920ed906cd9e81a90efaef8a8f4ca61ff60ae5f
soumen baidya <soumenbaidyaheart@gmail.com>
2018-03-07 12:42:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,23 +3,35 @@
 
 
 var button = document.getElementById('counter');
-var counter = 0;
+
 
 
 button.onclick = function () {
     
-    // Make a Request to a counter endpoint
+    // Create a Request object
+    
+    var request = new XMLHttpRequest();
     
     
     // capture the responce and store it in a variable
     
-    
-    //Render the variable in the correct span
-    
-    
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString(); 
+    request.onreadystatechange = function () {
+        if (request.readystate === XMLHttpRequest.DONE) {
+        // Take somen action
+            if(request.status === 200) {
+                var counter = requset.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString(); 
+            
+         }
+        }
+         //Not Done Yet
+        
+    };  
+     
+    // make the Request
+    requset.open('GET', 'http://soumenbaidya123.imad.hasura-app.io/counter', true);
+    request.send(null);
     
 };
 
